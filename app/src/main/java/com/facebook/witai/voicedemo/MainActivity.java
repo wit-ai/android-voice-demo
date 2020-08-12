a31aa2a95a80e3bb5e2aa246901860b517332d4b
Pan Wangperawong <itspanw@gmail.com>
2020-08-11 18:46:03 -0700

    Define audio settings and Runnable for recording and streaming to Wit Speech API

--- app/src/main/java/com/facebook/witai/voicedemo/MainActivity.java
+++ app/src/main/java/com/facebook/witai/voicedemo/MainActivity.java
@@ -8,17 +8,28 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import okhttp3.HttpUrl;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okio.BufferedSink;
 
 public class MainActivity extends AppCompatActivity {
     private Button speakButton;
@@ -28,6 +39,14 @@ public class MainActivity extends AppCompatActivity {
     private OkHttpClient httpClient;
     private HttpUrl.Builder httpBuilder;
     private Request.Builder httpRequestBuilder;
+
+    private AudioRecord recorder;
+    private static final int SAMPLE_RATE = 8000;
+    private static final int CHANNEL = AudioFormat.CHANNEL_IN_MONO;
+    private static final int AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT;
+    private static final int BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL, AUDIO_FORMAT) * 10;
+    private static final AtomicBoolean recordingInProgress = new AtomicBoolean(false);
+    private Thread recordingThread;
 
     /* Go to your Wit.ai app Management > Settings and obtain the Client Access Token */
     private static final String CLIENT_ACCESS_TOKEN = "<YOUR CLIENT ACCESS TOKEN>";
@@ -50,6 +69,60 @@ public class MainActivity extends AppCompatActivity {
         initializeHttpClient();
     }
 
+    // Define a Runnable to stream the recording data to the Speech API
+    // https://wit.ai/docs/http/20200513#post__speech_link
+    private class StreamRecordingRunnable implements Runnable {
+        @Override
+        public void run() {
+            final ByteBuffer buffer = ByteBuffer.allocateDirect(BUFFER_SIZE);
+            RequestBody requestBody = new RequestBody() {
+                @Override
+                public MediaType contentType() {
+                    return MediaType.parse("audio/raw;encoding=signed-integer;bits=16;rate=8000;endian=little");
+                }
+
+                @Override
+                public void writeTo(@NotNull BufferedSink bufferedSink) throws IOException {
+                    while (recordingInProgress.get()) {
+                        int result = recorder.read(buffer, BUFFER_SIZE);
+                        if (result < 0) {
+                            throw new RuntimeException("Reading of audio buffer failed: " +
+                                    getBufferReadFailureReason(result));
+                        }
+                        bufferedSink.write(buffer);
+                        buffer.clear();
+                    }
+                }
+            };
+
+            // Start streaming audio to Wit.ai Speech API
+            Request request = httpRequestBuilder.post(requestBody).build();
+            try (Response response = httpClient.newCall(request).execute()) {
+                if (response.isSuccessful()) {
+                    String responseData = response.body().string();
+                    /* To DO: respond to user after receiving a response back from Wit */
+                    Log.v("Streaming Response", responseData);
+                }
+            } catch (IOException e) {
+                Log.e("Streaming Response", e.getMessage());
+            }
+        }
+
+        private String getBufferReadFailureReason(int errorCode) {
+            switch (errorCode) {
+                case AudioRecord.ERROR_INVALID_OPERATION:
+                    return "ERROR_INVALID_OPERATION";
+                case AudioRecord.ERROR_BAD_VALUE:
+                    return "ERROR_BAD_VALUE";
+                case AudioRecord.ERROR_DEAD_OBJECT:
+                    return "ERROR_DEAD_OBJECT";
+                case AudioRecord.ERROR:
+                    return "ERROR";
+                default:
+                    return "Unknown (" + errorCode + ")";
+            }
+        }
+    }
 
     // Instantiate a Request.Builder that can be used for all the streaming requests
     // https://square.github.io/okhttp/recipes/#post-streaming-kt-java
