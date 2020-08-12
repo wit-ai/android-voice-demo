132e105192825937f0a5556c420427573155769a
Pan Wangperawong <itspanw@gmail.com>
2020-08-11 17:38:29 -0700

    Update MainActivity with check and request for permissions

--- app/src/main/java/com/facebook/witai/voicedemo/MainActivity.java
+++ app/src/main/java/com/facebook/witai/voicedemo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.facebook.witai.voicedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (!checkPermissionsFromDevice()) requestPermissions();
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.INTERNET,
+                Manifest.permission.RECORD_AUDIO
+        }, 1000);
+    }
+
+    private boolean checkPermissionsFromDevice() {
+        int recordAudioResult = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+        int internetResult = ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
+
+        return recordAudioResult == PackageManager.PERMISSION_GRANTED
+                && internetResult == PackageManager.PERMISSION_GRANTED;
     }
 }
