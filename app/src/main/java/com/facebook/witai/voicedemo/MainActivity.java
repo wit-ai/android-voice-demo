b074c045db09dca4ad387bca9c59aaddd62042ab
Pan Wangperawong <itspanw@gmail.com>
2020-08-11 18:30:51 -0700

    Add UI to layout and TextToSpeech API

--- app/src/main/java/com/facebook/witai/voicedemo/MainActivity.java
+++ app/src/main/java/com/facebook/witai/voicedemo/MainActivity.java
@@ -5,10 +5,21 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+    private Button speakButton;
+    private TextView speechTranscription;
+    private TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +27,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         if (!checkPermissionsFromDevice()) requestPermissions();
+
+        // Get a reference to the TextView and Button from the UI
+        speechTranscription = findViewById(R.id.speechTranscription);
+        speakButton = findViewById(R.id.speakButton);
+
+        // Initialize TextToSpeech
+        initializeTextToSpeech(this.getApplicationContext());
+    }
+
+    // Initialize the Android TextToSpeech
+    // https://developer.android.com/reference/android/speech/tts/TextToSpeech
+    private void initializeTextToSpeech(Context applicationContext) {
+        textToSpeech = new TextToSpeech(applicationContext, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int ttsStatus) {
+                // Disable the speakButton and provide the status of app while waiting for TextToSpeech to initialize
+                speechTranscription.setHint("Loading app ...");
+                speakButton.setEnabled(false);
+
+                // Check the status of the initialization
+                if (ttsStatus == TextToSpeech.SUCCESS) {
+                    speechTranscription.setHint("Press Speak and say something!");
+                    speakButton.setEnabled(true);
+                } else {
+                    String message = "TextToSpeech initialization failed";
+                    speechTranscription.setTextColor(Color.RED);
+                    speechTranscription.setText(message);
+                    Log.e("TextToSpeech", message);
+                }
+            }
+        });
     }
 
     private void requestPermissions() {
